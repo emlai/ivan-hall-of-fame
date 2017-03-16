31e59a38903cd452a8f8cb78d19d8234f4a1a44e
emlai <emil@cs.helsinki.fi>
2017-03-16 23:33:24 +0200

    Add route for getting high-scores in CSV format

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,4 +1,9 @@
 class ScoresController < ApplicationController
   def index
   end
+
+  def csv
+    csv_string = Score.sorted.map { |s| "\n#{s.points},\"#{s.character}\"" }.join
+    render plain: csv_string
+  end
 end
