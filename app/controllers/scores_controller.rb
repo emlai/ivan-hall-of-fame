0deb60f3598eee843f15d7bc2e5b21af4555b53c
emlai <emil@cs.helsinki.fi>
2017-04-20 18:06:05 +0300

    Submit score as anonymous if user authentication fails

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -11,11 +11,8 @@ class ScoresController < ApplicationController
   end
 
   def submit_score
-    player = Player.find_by(name: params[:username])
-    if params[:username].present? && (!player || params[:auth_token] != player.auth_token)
-      return head :unauthorized, content_type: "text/html"
-    end
-
+    player = Player.find_by(name: params[:username],
+                            auth_token: params[:auth_token])
     character, entry = params[:entry].split(", ", 2)
     version = Version.find_or_create_by(string: params[:version])
     Score.create(player: player, points: params[:score], character: character,
